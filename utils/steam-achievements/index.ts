b181a19c3c17f24b1097c9d0311aa727d7b23ef0
Esther <151816213+SterTheStar@users.noreply.github.com>
2025-10-01 17:24:32 -0300

    Delete utils/steam-achievements directory

--- utils/steam-achievements/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './types';
-export * from './parser';
-export * from './monitor';
-export * from './hydra-api';
-export * from './steam-api';
-export * from './writer';
-export * from './unlock';
