a57f8dd2df6e3e55d5e86eaeaaf9be5438ca219d
Esther <151816213+SterTheStar@users.noreply.github.com>
2026-02-16 21:32:37 -0300

    :)
    Just to upload new code

--- src-tauri/src/api/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod hydra;
-pub mod steam;
-
-pub use hydra::HydraAPI;
-pub use steam::SteamAPI;
--- src-tauri/src/utils/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod exporter;
-pub mod http;
-pub mod cache;
-
-pub use exporter::AchievementExporter;
-pub use cache::CacheManager;
